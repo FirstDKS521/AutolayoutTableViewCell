9651d5e3075509eb15c40d02f61d49cd5b5f0c7e
duks <duks@robooot.com>
2017-01-21 13:11:10 +0800

    Initial Commit

---
+++ AutolayoutTableViewCell/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutolayoutTableViewCell
+//
+//  Created by aDu on 2017/1/21.
+//  Copyright © 2017年 DuKaiShun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
